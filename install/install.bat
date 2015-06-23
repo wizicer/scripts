f9a37acd1cabfd0f4fb7776622620324197f189f
Icer <liangshuangde@163.com>
2015-06-23 17:02:36 +0800

    add -y for choco install

--- install/install.bat
+++ install/install.bat
@@ -27,39 +27,39 @@ if '%errorlevel%' NEQ '0' (
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install git.install
-choco install git-credential-winstore
-choco install vim
-choco install conemu
-choco install python2
-choco install putty.portable
-choco install autohotkey.portable
-choco install foobar2000
-choco install treesizefree
-choco install opera
-choco install google-chrome-x64
-choco install everything
-choco install linkshellextension
-choco install nodejs.install
-choco install wp-project-screen
-choco install tightvnc
-choco install 7zip.install
-choco install winsplit
+choco install git.install -y
+choco install git-credential-winstore -y
+choco install vim -y
+choco install conemu -y
+choco install python2 -y
+choco install putty.portable -y
+choco install autohotkey.portable -y
+choco install foobar2000 -y
+choco install treesizefree -y
+choco install opera -y
+choco install google-chrome-x64 -y
+choco install everything -y
+choco install linkshellextension -y
+choco install nodejs.install -y
+choco install wp-project-screen -y
+choco install tightvnc -y
+choco install 7zip.install -y
+choco install winsplit -y
 
-rem choco install clcl
-rem choco install unlocker
-rem choco install applocale
+rem choco install clcl -y
+rem choco install unlocker -y
+rem choco install applocale -y
 
-choco install sysinternals
-choco install winscp.install
-choco install ilspy
-choco install snoop
-choco install pandoc
-choco install imagemagick
-choco install plantuml
-choco install graphviz
-choco install fiddler
-choco install torgoisegit
+choco install sysinternals -y
+choco install winscp.install -y
+choco install ilspy -y
+choco install snoop -y
+choco install pandoc -y
+choco install imagemagick -y
+choco install plantuml -y
+choco install graphviz -y
+choco install fiddler -y
+choco install torgoisegit -y
 
 git clone https://github.com/wizicer/dotvim.git %USERPROFILE%\vimfiles
 cd /d %USERPROFILE%\vimfiles
