586254e8f7d232ac036f51f5a755aac9ecf11bce
Icer <liangshuangde@163.com>
2015-06-25 10:15:26 +0800

    update git script with assume command

--- install/setupgit.sh
+++ install/setupgit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global alias.s "!git status && echo ========= && git lg -9 && echo ========= && git config --get-regexp remote.*.url && echo ========= && git config --get-regexp user.*"
+git config --global alias.s "!git status && echo ========= && git lg -9 && echo ========= && git config --get-regexp remote.*.url && echo ========= && git config --get-regexp user.* && echo ========= && git ls-files -v | grep '^[a-z]' | cut -c3-"
 git config --global alias.l "!git lg -9"
 git config --global alias.co "checkout"
 git config --global alias.ci "commit"
@@ -12,4 +12,8 @@ git config --global alias.diffc "diff --cached"
 git config --global alias.showalias "!git config -l | grep alias | cut -c 7-"
 git config --global alias.addi "add -i"
 git config --global alias.initialize "!git add -A && git commit -m 'initial check in'"
+git config --global alias.root "!gitroot"
+git config --global alias.lsassume "!git ls-files -v | grep '^[a-z]' | cut -c3-"
+git config --global alias.assume "update-index --assume-unchanged"
+git config --global alias.unassume "update-index --no-assume-unchanged"
 git config --global url.https://.insteadOf git://
