05442a38dfe38a8bc11f0bb5c51cb98b6fc4590c
Icer <liangshuangde@163.com>
2015-07-23 14:07:52 +0800

    add proxy setting for nuget

--- install/proxy.sh
+++ install/proxy.sh
@@ -5,3 +5,8 @@ npm config set proxy http://127.0.0.1:1080
 
 git config --global http.proxy 'socks5://127.0.0.1:1080'
 git config --global https.proxy 'socks5://127.0.0.1:1080'
+
+curl -L -O https://www.nuget.org/nuget.exe -x http://127.0.0.1:1080
+nuget.exe config -set http_proxy=http://127.0.0.1:1080
+# nuget.exe config -set http_proxy.user=mydomain\myUserName
+# nuget.exe config -set http_proxy.password=mySuperSecretPassword
