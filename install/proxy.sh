4b26e855911b8eb449ce9e2f8fea22a361ca5882
Icer <liangshuangde@163.com>
2015-07-23 14:15:21 +0800

    change git proxy to http proxy

--- install/proxy.sh
+++ install/proxy.sh
@@ -3,8 +3,8 @@
 npm config set https-proxy http://127.0.0.1:1080
 npm config set proxy http://127.0.0.1:1080
 
-git config --global http.proxy 'socks5://127.0.0.1:1080'
-git config --global https.proxy 'socks5://127.0.0.1:1080'
+git config --global http.proxy 'http://127.0.0.1:1080'
+git config --global https.proxy 'http://127.0.0.1:1080'
 
 curl -L -O https://www.nuget.org/nuget.exe -x http://127.0.0.1:1080
 nuget.exe config -set http_proxy=http://127.0.0.1:1080
