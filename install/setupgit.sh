45935c65a8b72756f22ffc9b7cef1d5aa0ac2bc1
Icer <liangshuangde@163.com>
2015-08-04 13:29:05 +0800

    enhance status for origin and author

--- install/setupgit.sh
+++ install/setupgit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global alias.s "!git status && echo ========= && git lg -9 && echo ========= && git config --get-regexp remote.*.url && echo ========= && git config --get-regexp user.* && echo ========= && git ls-files -v | grep '^[a-z]' | cut -c3-"
+git config --global alias.s "!git status && echo ========= && git lg -9 && echo ========= && git remote -v && echo Author: \`git config user.name\` \<\`git config user.email\`\> && echo ========= && git ls-files -v | grep '^[a-z]' | cut -c3-"
 git config --global alias.l "!git lg -9"
 git config --global alias.co "checkout"
 git config --global alias.ci "commit"
@@ -19,4 +19,7 @@ git config --global alias.makeexecutable "update-index --chmod=+x"
 git config --global alias.makeexec "update-index --chmod=+x"
 git config --global alias.initvsignore "!curl https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore>.gitignore"
 git config --global alias.initnodeignore "!curl https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore>.gitignore"
+git config --global alias.mergetheirs "merge --strategy-option theirs"
+git config --global alias.mergeours "merge --strategy-option ours"
+git config --global alias.showname "show --name-only"
 git config --global url.https://.insteadOf git://
