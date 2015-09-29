5f6b2bf41293c96973b013738bbf6de59abab7d6
Icer <liangshuangde@163.com>
2015-09-29 10:19:48 +0800

    change some proxy to socks(backed by MyEnTunnel and privoxy)

--- install/proxy.sh
+++ install/proxy.sh
@@ -3,10 +3,10 @@
 npm config set https-proxy http://127.0.0.1:1080
 npm config set proxy http://127.0.0.1:1080
 
-git config --global http.proxy 'http://127.0.0.1:1080'
-git config --global https.proxy 'http://127.0.0.1:1080'
+git config --global http.proxy 'socks5://127.0.0.1:7070'
+git config --global https.proxy 'socks5://127.0.0.1:7070'
 
-curl -L -O https://www.nuget.org/nuget.exe -x http://127.0.0.1:1080
-nuget.exe config -set http_proxy=http://127.0.0.1:1080
+curl -L -O https://www.nuget.org/nuget.exe --socks5-hostname 127.0.0.1:1080
+./nuget.exe config -set http_proxy=http://127.0.0.1:1080
 # nuget.exe config -set http_proxy.user=mydomain\myUserName
 # nuget.exe config -set http_proxy.password=mySuperSecretPassword
