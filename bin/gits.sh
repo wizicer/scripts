17320483f9fc113bb8b174504b82b4fbf6044ec3
Icer <liangshuangde@163.com>
2016-12-19 15:34:44 +0800

    enhance gits command with custom arguments

--- bin/gits.sh
+++ bin/gits.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+RED='\033[0;34m'
+NC='\033[0m' # No Color
+command=${*:-status -sb}
+[ $# -eq 0 ] && compact=-n
+
 for gitdir in `find ./ -type d -name .git -maxdepth 3`; 
     do 
-        echo $1, $*, $#
         workdir=${gitdir%/*}; 
-        echo -n "$workdir  "; 
+        echo -e ${compact} " ${RED}$workdir${NC}  "; 
         # git --git-dir=$gitdir --work-tree=$workdir fap; 
-        git --git-dir=$gitdir --work-tree=$workdir status -sb; 
+        git --git-dir=$gitdir --work-tree=$workdir $command; 
     done
 
 #credit to http://stackoverflow.com/a/2999415/2558077
