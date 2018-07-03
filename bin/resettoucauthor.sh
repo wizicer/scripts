3f3f32a70c49487818553ed273326e37618dae02
Icer <liangshuangde@163.com>
2018-07-04 06:47:16 +0800

    update reset script file privilege and add reset to uc script

--- bin/resettoucauthor.sh
+++ bin/resettoucauthor.sh
@@ -1,9 +1,9 @@
-git config user.email liangshuangde@163.com
+git config user.email liangshuang@uchaindb.com
 git filter-branch --commit-filter \
 'export GIT_AUTHOR_NAME="Icer";\
-export GIT_AUTHOR_EMAIL=liangshuangde@163.com;\
+export GIT_AUTHOR_EMAIL=liangshuang@uchaindb.com;\
 export GIT_COMMITTER_NAME="Icer";\
-export GIT_COMMITTER_EMAIL=liangshuangde@163.com;\
+export GIT_COMMITTER_EMAIL=liangshuang@uchaindb.com;\
 git commit-tree "$@"'
 
 #if need to change author according to some criteria, here is the clue: http://stackoverflow.com/a/4494037/2558077
